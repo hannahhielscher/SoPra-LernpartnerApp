6da5e993408af1f45ffbcfef70dda30b82842d83
AleynaOezdal <79413535+AleynaOezdal@users.noreply.github.com>
2021-05-06 12:51:27 +0200

    Update Teilnahme.py

--- src/server/bo/Konversation.py
+++ src/server/bo/Konversation.py
@@ -9,10 +9,23 @@ class Konversation (bo.BusinessObject):
         self._teilnehmer= list
 
     def get_nachricht(self):
+        """Auslesen der Nachricht"""
         return self._nachricht
 
     def get_teilnehmer(self):
-        return self.
+        """Auslesen von Teilnehmern"""
+        return self._teilnehmer
 
     def set_teilnehmer(self):
-        self._teilnehmer= teilnehmer_neu(list)
+        """Setzten von neuen Teilnehmern"""
+        self._teilnehmer= list
+    
+     @staticmethod
+    def from_dict(dictionary=dict()):
+        obj = Konversation()
+        obj.set_id(dictionary["id"])  # part of the Business object mother class
+        obj.set_name(dictionary["name"])
+        obj.set_vorname(dictionary["vorname"])
+        return obj
+
+    
--- src/server/bo/Teilnahme.py
+++ src/server/bo/Teilnahme.py
@@ -8,7 +8,18 @@ class Teilnahme (bo.BusinessObject):
         self._status = bool
 
     def get_status(self):
+        """Auslesen vom Status"""
         return self._status
 
     def set_status(self, person):
-        self._status= status_neu(bool)
+        """aktuellen Status setzten"""
+        self._status= bool
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+         """Umwandeln eines Python dict() in einen User()."""
+        obj = Teilnahme()
+        obj.set_id(dictionary["id"])  # part of the Business object mother class
+        obj.set_name(dictionary["name"])
+        obj.set_vorname(dictionary["vorname"])
+        return obj
