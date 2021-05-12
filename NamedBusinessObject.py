4e00bb105e16751c62b8609610aa3192250f4f14
sinariedel <83543691+sinariedel@users.noreply.github.com>
2021-05-12 08:54:44 +0200

    Add files via upload

---
+++ NamedBusinessObject.py
@@ -0,0 +1,12 @@
+from server.bo import BusinessObject
+
+class NamedBusinessObject:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return str
+
+    def set_name(self):
+        return str
---
+++ Vorschlag.py
@@ -0,0 +1,17 @@
+from server.bo import BusinessObject as bo
+
+class Vorschlag():
+
+    def __init__(self):
+        self.list= list
+
+    def get_vorschlag(self):
+        return
+
+    def get_matches(self):
+        return list
+
+    def set_vorschlag(self):
+        return list
+
+
