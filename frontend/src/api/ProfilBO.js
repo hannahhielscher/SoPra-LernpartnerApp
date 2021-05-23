8f88a225a9acb960005ae30ee2539c35d6227278
Hannah Hielscher <hh057@hdm-stuttgart.de>
2021-05-23 10:22:29 +0200

    Create ProfilBO.js

---
+++ frontend/src/api/ProfilBO.js
@@ -0,0 +1,12 @@
+import BusinessObject from './BusinessObject';
+
+/**
+ * Repräsentiert ein Profil Objekt einer Person .
+ */
+export default class ProfilBO extends BusinessObject {
+    /**
+   * Constructs a new ProfilBO object with a given owner.
+   * 
+   * @param {*} aOwner - the owner of this ProfilBO.
+   */
+}
