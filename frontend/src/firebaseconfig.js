b130870ead78443abb0072b95cd3196cfb44db6e
Hannah Hielscher <hh057@hdm-stuttgart.de>
2021-05-23 10:11:28 +0200

    Create firebaseconfig.js

---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,17 @@
+/** The firebase config structure for the Python Bank Project as provided by the firebase admin website */
+//import firebase from "firebase/app"
+//import "firebase/auth";
+//import "firebase/firestore";
+
+const firebaseConfig = {
+    /* Fill in your Data ! */
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
+  };
+  
+  export default firebaseConfig;
