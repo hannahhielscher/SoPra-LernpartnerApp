085025ed80e6688a552558734cfb16a3022524d7
mariavetter <83543620+mariavetter@users.noreply.github.com>
2021-05-26 11:08:47 +0200

    Update BO

---
+++ src/server/bo/TeilnahmeGruppe.py
@@ -0,0 +1,46 @@
+from server.bo import BusinessObject as bo
+
+
+class Teilnahme (bo.BusinessObject):
+    
+    def __init__(self):
+        super().__init__()
+        self._status = False
+        self._teilnehmer= None
+        self._lerngruppe= None
+
+    def get_teilnehmer(self):
+        """Auslesen des Teilnehmers"""
+        return self._teilnehmer
+    
+    def set_teilnehmer(self, teilnehmer):
+        """setzten des Teilnehmers"""
+        self._teilnehmer= teilnehmer
+
+    def get_lerngruppe(self):
+        """Auslesen der Lerngruppe"""
+        return self._lerngruppe
+    
+    def set_lerngruppe(self, lerngruppe):
+        """setzten der neuen Lerngruppe"""
+        self._lerngruppe= lerngruppe
+
+
+    def get_status(self):
+        """Auslesen vom Status"""
+        return self._status
+
+    def set_status(self, value):
+        """aktuellen Status setzten"""
+        self._status= value
+        
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+         """Umwandeln eines Python dict() in einen User()."""
+        obj = Teilnahme()
+        obj.set_id(dictionary["id"])  # part of the Business object mother class
+        obj.set_status(dictionary['status'])
+        obj.set_lerngruppe(dictionary['lerngruppe'])
+        obj.set_teilnehmer(dictionary['teilnehmer'])
+        return obj
