9067dccd6ad487834d89b4b53217737055ae30f1
Hannah Hielscher <hh057@hdm-stuttgart.de>
2021-05-26 18:21:59 +0200

    Update TeilnahmeChat.py

--- src/server/bo/TeilnahmeChat.py
+++ src/server/bo/TeilnahmeChat.py
@@ -5,24 +5,24 @@ class TeilnahmeChat(bo.BusinessObject):
     
     def __init__(self):
         super().__init__()
-        self._person = False
+        self._teilnehmer = False
         self._konversation = None
 
-    def get_person(self):
+    def get_teilnehmer(self):
         """Auslesen des Teilnehmers"""
-        return self._person
+        return self._teilnehmer
     
-    def set_person(self, person_id):
+    def set_teilnehmer(self, value):
         """setzten des Teilnehmers"""
-        self._teilnehmer= person_id
+        self._teilnehmer= value
 
     def get_konversation(self):
         """Auslesen der Lerngruppe"""
         return self._konversation
     
-    def set_konversation(self, konversation_id):
+    def set_konversation(self, value):
         """setzten der neuen Lerngruppe"""
-        self._konversation = konversation_id
+        self._konversation = value
         
 
     @staticmethod
