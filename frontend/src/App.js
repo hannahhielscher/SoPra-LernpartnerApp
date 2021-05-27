136a582e732ab0b406c16b27b9cee06f0b119b97
Hannah Hielscher <hh057@hdm-stuttgart.de>
2021-05-27 12:53:02 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import Header from './components/layout/header';
 import { Paper, Typography, Tabs, Tab } from '@material-ui/core';
 //import { Link as RouterLink } from 'react-router-dom';
 //import ProfileDropDown from '../dialogs/ProfileDropDown';
+import About from './components/pages/About';
+import SignIn from './components/pages/SignIn';
+import LoadingProgress from './components/dialogs/LoadingProgress';
+import ContextErrorMessage from './components/dialogs/ContextErrorMessage';
+import firebaseConfig from './firebaseconfig';
 
 function App() {
   return (
