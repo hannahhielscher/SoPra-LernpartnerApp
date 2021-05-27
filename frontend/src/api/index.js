f0e794423d1aa575a1bc42ddb62db15ddf6e8a46
Hannah Hielscher <hh057@hdm-stuttgart.de>
2021-05-27 13:11:50 +0200

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,3 +2,5 @@
  * Reexports the Bussines Objects for a well-encaspulated Api module. 
  * Hier kommen später die exports für alle BOs rein in dem Ordner (außer Superklasse)
  */
+export {default as LernpartnerAPI} from './LernpartnerAPI';
+export {default as PersonBO} from './PersonBO';
