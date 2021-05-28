3f0a3567fff7a658b56d9a280dbacaaab5cadf9e
sinariedel <sr167@hdm-stuttgart.de>
2021-05-28 09:51:40 +0200

    Update Vorschlag

--- src/server/bo/Vorschlag.py
+++ src/server/bo/Vorschlag.py
@@ -4,10 +4,12 @@ class Vorschlag():
 
     def __init__(self, matches):
         self._matches = matches
+        """Auslesen der Matches"""
 
     def get_matches(self):
         for i in self._matches:
             vorschlag = i
             return vorschlag
+        """Setzen der Vorschläge"""
 
 
