5f9aee4f5090b4f84aaf3336335933d1f33475ef
AleynaOezdal <79413535+AleynaOezdal@users.noreply.github.com>
2021-05-31 16:22:24 +0200

    Create TeilnahmeChatBO.js

---
+++ frontend/src/api/TeilnahmeChatBO.js
@@ -0,0 +1,57 @@
+import BusinessObject from '/.BusinessObject';
+
+export default class TeilnahmeChatBO extends BusinessObject{
+
+	constructor(ateilnehmer, akonversation){
+        super();
+        this.teilnehmer = ateilnehmer;
+        this.koversation = akonversation;
+    }
+
+
+    /*
+	erhalte 
+	*/
+    getteilnehmer(){
+        return this.teilnehmer;
+    }
+    /*
+	setze 
+	*/
+    setteilnehmer(ateilnehmer){
+        this.teilnehmer = ateilnehmer;
+    }
+
+      /*
+	erhalte 
+	*/
+    getkonversation(){
+        return this.konversation;
+    }
+
+    /*
+	setze 
+	*/
+    setkonversation(akonversation){
+        this.konversation = akonversation;
+    }
+    
+
+    static fromJSON(teilnahmeChats) {
+		let results = null;
+		if (Array.isArray(teilnahmeChats)) {
+			results = [];
+			teilnahmeChats.forEach((c) => {
+				Object.setPrototypeOf(c, TeilnahmeChatsBO.prototype);
+				results.push(c);
+			})
+		} else {
+			// Es gibt wohl nur ein Objekt
+			let c = teilnahmeChats;
+			Object.setPrototypeOf(c, TeilnahmeChatsBO.prototype);
+			results = c;
+		}
+		return results;
+	}
+} 
+
