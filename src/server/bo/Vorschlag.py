6ea1a70909780051e171001abf2807cabcac770c
mariavetter <83543620+mariavetter@users.noreply.github.com>
2021-06-02 12:02:05 +0200

    Update Vorschlag.py

--- src/server/bo/Vorschlag.py
+++ src/server/bo/Vorschlag.py
@@ -4,9 +4,18 @@ class Vorschlag():
 
     def __init__(self, match):
         self._match_quote = match
+        self._profil_id = None
 
-    """Auslesen der Matches"""
     def get_match(self):
+        """Auslesen der Matches"""
         return self._match_quote
+
+    def get_profil_id(self):
+        """Auslesen des Personenprofils"""
+        return self._profil_id
+
+    def set_profil_id(self, value):
+        """Setzen eines Lernprofils (geht das überhaupt?)"""
+        self._profil_id = value
 
 
