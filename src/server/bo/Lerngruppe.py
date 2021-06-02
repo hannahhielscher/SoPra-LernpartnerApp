7dc4a5ab770116f7c3bad9fb37c05d169ff54bb5
mariavetter <83543620+mariavetter@users.noreply.github.com>
2021-06-02 14:47:52 +0200

    Update Lerngruppe.py

--- src/server/bo/Lerngruppe.py
+++ src/server/bo/Lerngruppe.py
@@ -6,24 +6,22 @@ class Lerngruppe(NamedBusinessObject):
         super().__init__()
         self._teilnehmer = []
         self._gruppenprofil = Profil
-        self._lernvorlieben = Lernvorlieben
-
-
-    def set_teilnehmer(self, teilnehmer):
-        """Setzen eines Teilnehmers"""
-        self._teilnehmer = teilnehmer
 
     def get_teilnehmer(self):
         """Auslesen eines Teilnehmers"""
         return self._teilnehmer
 
-    def set_gruppenprofil(self, gruppenprofil):
-        """Setzen eines Gruppenprofils"""
-        self._gruppenprofil = gruppenprofil
+    def set_teilnehmer(self, teilnehmer):
+        """Setzen eines Teilnehmers"""
+        self._teilnehmer = teilnehmer
 
     def get_gruppenprofil(self):
         """Auslesen eines Gruppenprofils"""
         return self._gruppenprofil
+
+    def set_gruppenprofil(self, gruppenprofil):
+        """Setzen eines Gruppenprofils"""
+        self._gruppenprofil = gruppenprofil
 
     @staticmethod
     def from_dict(dictionary=dict()):
