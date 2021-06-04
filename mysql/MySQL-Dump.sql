2028f056b378e7f341bf5d674af594d079a1939d
mariavetter <83543620+mariavetter@users.noreply.github.com>
2021-06-04 23:22:23 +0200

    Update MySQL-Dump.sql

--- mysql/MySQL-Dump.sql
+++ mysql/MySQL-Dump.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `lernapp_SWPra` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `lernapp_SWPra` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `lernapp_SWPra`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,33 +12,13 @@ USE `lernapp_SWPra`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Table `lernapp_SWPra`.`lernvorlieben`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `lernapp_SWPra`.`lernvorlieben` ;
-
-CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`lernvorlieben` (
-  `id` INT NOT NULL,
-  `tageszeit` INT NULL,
-  `tage` INT NULL,
-  `frequenz` INT NULL,
-  `lernart` INT NULL,
-  `gruppengroesse` INT NULL,
-  `lernort` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
 -- -----------------------------------------------------
 -- Table `lernapp_SWPra`.`profile`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `lernapp_SWPra`.`profile` ;
-
-CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`profile` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lernapp_SWPra`.`profile` (
   `id` INT NOT NULL,
   `studiengang` VARCHAR(45) NULL,
   `semester` INT NULL,
@@ -50,15 +30,18 @@ CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`profile` (
     REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `module`
+-- Dumping data for table `profile`
 --
 
-LOCK TABLES `profile` WRITE;
-/*!40000 ALTER TABLE `module` DISABLE KEYS */;
-INSERT INTO `profile` VALUES (1, 'Wirtschaftsinformatik und digitale Medien', 3, 1), (2, 'CR/PR', 2, 2)/*!40000 ALTER TABLE `module` ENABLE KEYS */;
+LOCK TABLES `lernapp_SWPra`.`profile` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`profile` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`profile` VALUES (1, 'Wirtschaftsinformatik und digitale Medien', 3, 1), (2, 'CR/PR', 2, 2), (3, 'Wirtschaftsinformatik und digitale Medien', 2, 3),
+(4, 'Wirtschaftsinformatik und digitale Medien', 3, 4), (5, 'CR/PR', 2, 5), (6, 'Medieninformatik', 5, 6), (7, 'Medieninformatik', 3, 7);
+/*!40000 ALTER TABLE `lernapp_SWPra`.`profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
 -- -----------------------------------------------------
@@ -66,14 +49,13 @@ UNLOCK TABLES;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `lernapp_SWPra`.`personen` ;
 
-CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`personen` (
+CREATE TABLE `lernapp_SWPra`.`personen` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
+  `name` VARCHAR(200) NULL,
   `vorname` VARCHAR(45) NULL,
-  `semester` INT NULL,
   `alter` INT NULL,
   `geschlecht` VARCHAR(45) NULL,
-  `lerngruppe` VARCHAR(45) NULL,
+  `lerngruppe` TINYINT,
   `google_user_id` VARCHAR(45) NULL,
   `email` VARCHAR(45) NULL,
   `profil_id` INT NOT NULL,
@@ -86,6 +68,17 @@ CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`personen` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+--
+-- Dumping data for table `personen`
+--
+
+LOCK TABLES `lernapp_SWPra`.`personen` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`personen` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`personen` VALUES (1, 'Müller', 'Tim', 20, 'männlich', true, '1', 'timmueller@gmail.com', 1), (2, 'Maier', 'Lisa', 19, 'weiblich', true, '2', 'lischen2002@gmx.de', 2),
+(3, 'Baum', 'Manuela', 22, 'weiblich', false,  '3', 'manni99@gmail.com', 3), (4, 'Possible', 'Kim', 20, 'weiblich', true, '4', 'kimpossible@hotmail.de', 4), (5, 'Smith', 'John', 24, 'männlich', true, '5', 'jonny@hotmail.com', 5), 
+(6, 'Jones', 'Alex', 23, 'männlich', false, '6', 'alexjones@gmail.com', 6),  (7, 'Becker', 'Klaus', 19, 'männlich', true, '7', 'becker77@gmx.de', 7);
+/*!40000 ALTER TABLE `lernapp_SWPra`.`personen` ENABLE KEYS */;
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `lernapp_SWPra`.`lernfaecher`
@@ -98,24 +91,16 @@ CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`lernfaecher` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+--
+-- Dumping data for table `lernfaecher`
+--
 
--- -----------------------------------------------------
--- Table `lernapp_SWPra`.`person_has_lernfaecher`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `lernapp_SWPra`.`person_has_lernfaecher` ;
-
-CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`person_has_lernfaecher` (
-  `lernfaecher_id_lernfaecher` INT NOT NULL,
-  `kenntnisstand` VARCHAR(45) NULL,
-  PRIMARY KEY (`lernfaecher_id_lernfaecher`),
-  INDEX `fk_person_has_courses_courses1_idx` (`lernfaecher_id_lernfaecher` ASC) VISIBLE,
-  CONSTRAINT `fk_person_has_courses_courses1`
-    FOREIGN KEY (`lernfaecher_id_lernfaecher`)
-    REFERENCES `lernapp_SWPra`.`lernfaecher` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+LOCK TABLES `lernapp_SWPra`.`lernfaecher` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernfaecher` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`lernfaecher` VALUES (1, 'Software Entwicklung'), (2, 'Data Science'), (3, 'Rechnungswesen'), (4, 'Medienrecht'),
+(5, 'Crossmedia-Konzeption'), (6, 'Web-Technologie und Datenkompetenz'), (7, 'Datenbanken'), (8, 'IT-Security');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernfaecher` ENABLE KEYS */;
+UNLOCK TABLES;
 
 -- -----------------------------------------------------
 -- Table `lernapp_SWPra`.`lerngruppen`
@@ -160,7 +145,6 @@ CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`profile_has_lernfaecher` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `lernapp_SWPra`.`teilnahmen_gruppe`
@@ -228,25 +212,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lernapp_SWPra`.`tage`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `lernapp_SWPra`.`tage` ;
-
-CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`tage` (
-  `id` INT NOT NULL,
-  `präferenz` VARCHAR(45) NULL,
-  `lernvorlieben_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_tage_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_tage_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `lernapp_SWPra`.`tageszeiten`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `lernapp_SWPra`.`tageszeiten` ;
@@ -254,15 +219,40 @@ DROP TABLE IF EXISTS `lernapp_SWPra`.`tageszeiten` ;
 CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`tageszeiten` (
   `id` INT NOT NULL,
   `präferenz` VARCHAR(45) NULL,
-  `lernvorlieben_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_tageszeit_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_tageszeit_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+--
+-- Dumping data for table `tageszeiten`
+--
+
+LOCK TABLES `lernapp_SWPra`.`tageszeiten` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`tageszeiten` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`tageszeiten` VALUES (1, 'Morgens'), (2, 'Mittags'), (3, 'Abends');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`tageszeiten` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+-- -----------------------------------------------------
+-- Table `lernapp_SWPra`.`tage`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lernapp_SWPra`.`tage` ;
+
+CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`tage` (
+  `id` INT NOT NULL,
+  `präferenz` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+--
+-- Dumping data for table `tage`
+--
+
+LOCK TABLES `lernapp_SWPra`.`tage` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`tage` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`tage` VALUES (1, 'Unter der Woche'), (2, 'Am Wochenende');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`tage` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 -- -----------------------------------------------------
@@ -272,16 +262,19 @@ DROP TABLE IF EXISTS `lernapp_SWPra`.`frequenzen` ;
 
 CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`frequenzen` (
   `id` INT NOT NULL,
-  `präferenz` VARCHAR(45) NULL,
-  `lernvorlieben_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_frequenz_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_frequenz_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `präferenz` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+--
+-- Dumping data for table `frequenzen`
+--
+
+LOCK TABLES `lernapp_SWPra`.`frequenzen` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`frequenzen` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`frequenzen` VALUES (1, 'Wöchentlich'), (2, 'Mehrmals die Woche'), (3, 'Alle zwei Wochen');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`frequenzen` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 -- -----------------------------------------------------
@@ -291,16 +284,19 @@ DROP TABLE IF EXISTS `lernapp_SWPra`.`lernarten` ;
 
 CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`lernarten` (
   `id` INT NOT NULL,
-  `lernvorlieben_id` INT NOT NULL,
   `präferenz` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_lernart_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_lernart_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+--
+-- Dumping data for table `lernarten`
+--
+
+LOCK TABLES `lernapp_SWPra`.`lernarten` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernarten` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`lernarten` VALUES (1, 'Visuell'), (2, 'Auditiv'), (3, 'Motorisch'), (4, 'Kommunikativ');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernarten` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 -- -----------------------------------------------------
@@ -310,16 +306,19 @@ DROP TABLE IF EXISTS `lernapp_SWPra`.`gruppengroessen` ;
 
 CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`gruppengroessen` (
   `id` INT NOT NULL,
-  `präferenz` VARCHAR(45) NULL,
-  `lernvorlieben_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_gruppengroessen_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_gruppengroessen_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `präferenz` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+--
+-- Dumping data for table `gruppengroessen`
+--
+
+LOCK TABLES `lernapp_SWPra`.`gruppengroessen` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`gruppengroessen` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`gruppengroessen` VALUES (1, 'Bis zu 3 Personen'), (2, '3-5 Personen'), (3, 'Über 5 Personen');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`gruppengroessen` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 -- -----------------------------------------------------
@@ -329,16 +328,85 @@ DROP TABLE IF EXISTS `lernapp_SWPra`.`lernorte` ;
 
 CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`lernorte` (
   `id` INT NOT NULL,
-  `präferenz` VARCHAR(45) NULL,
-  `lernvorlieben_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `lernvorlieben_id`),
-  INDEX `fk_lernorte_lernvorlieben1_idx` (`lernvorlieben_id` ASC) VISIBLE,
-  CONSTRAINT `fk_lernorte_lernvorlieben1`
-    FOREIGN KEY (`lernvorlieben_id`)
-    REFERENCES `lernapp_SWPra`.`lernvorlieben` (`id`)
+  `präferenz` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+--
+-- Dumping data for table `lernorte`
+--
+
+LOCK TABLES `lernapp_SWPra`.`lernorte` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernorte` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`lernorte` VALUES (1, 'Remote'), (2, 'Hochschule'), (3, 'Bibliothek'), (4, 'Cafe');
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernorte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+-- -----------------------------------------------------
+-- Table `lernapp_SWPra`.`lernvorlieben`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lernapp_SWPra`.`lernvorlieben` ;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `lernapp_SWPra`.`lernvorlieben` (
+  `id` INT NOT NULL,
+  `tageszeiten_id` INT NOT NULL,
+  `tage_id` INT NOT NULL,
+  `frequenzen_id` INT NOT NULL,
+  `lernarten_id` INT NOT NULL,
+  `gruppengroessen_id` INT NOT NULL,
+  `lernorte_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `tageszeiten_id`, `tage_id`, `frequenzen_id`, `lernarten_id`, `gruppengroessen_id`, `lernorte_id`),
+  INDEX `fk_lernvorlieben_tageszeiten1_idx` (`tageszeiten_id` ASC) VISIBLE,
+  INDEX `fk_lernvorlieben_tage1_idx` (`tage_id` ASC) VISIBLE,
+  INDEX `fk_lernvorlieben_frequenzen1_idx` (`frequenzen_id` ASC) VISIBLE,
+  INDEX `fk_lernvorlieben_lernarten1_idx` (`lernarten_id` ASC) VISIBLE,
+  INDEX `fk_lernvorlieben_gruppengroessen1_idx` (`gruppengroessen_id` ASC) VISIBLE,
+  INDEX `fk_lernvorlieben_lernorte1_idx` (`lernorte_id` ASC) VISIBLE,
+  CONSTRAINT `fk_lernvorlieben_tageszeiten1`
+    FOREIGN KEY (`tageszeiten_id`)
+    REFERENCES `lernapp_SWPra`.`tageszeiten` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lernvorlieben_tage1`
+    FOREIGN KEY (`tage_id`)
+    REFERENCES `lernapp_SWPra`.`tage` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lernvorlieben_frequenzen1`
+    FOREIGN KEY (`frequenzen_id`)
+    REFERENCES `lernapp_SWPra`.`frequenzen` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lernvorlieben_lernarten1`
+    FOREIGN KEY (`lernarten_id`)
+    REFERENCES `lernapp_SWPra`.`lernarten` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lernvorlieben_gruppengroessen1`
+    FOREIGN KEY (`gruppengroessen_id`)
+    REFERENCES `lernapp_SWPra`.`gruppengroessen` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lernvorlieben_lernorte1`
+    FOREIGN KEY (`lernorte_id`)
+    REFERENCES `lernapp_SWPra`.`lernorte` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lernvorlieben`
+--
+
+LOCK TABLES `lernapp_SWPra`.`lernvorlieben` WRITE;
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernvorlieben` DISABLE KEYS */;
+INSERT INTO `lernapp_SWPra`.`lernvorlieben` VALUES (1, 2, 1, 3, 2, 1, 1), (2, 2, 2, 2, 1, 1, 3), (3, 1, 1, 2, 3, 3, 4), 
+(4, 3, 1, 3, 4, 3, 4), (5, 1, 2, 1, 1, 3, 3), (6, 2, 2, 2, 2, 1, 4), (7, 3, 2, 2, 1, 2, 2);
+/*!40000 ALTER TABLE `lernapp_SWPra`.`lernvorlieben` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 -- -----------------------------------------------------
