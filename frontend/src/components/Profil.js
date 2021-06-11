67b78a604b994e5d781a7a6a02877e11a3f6f685
cassandrakoenitzer <ck181@hdm-stuttgart.de>
2021-06-11 16:58:37 +0200

    Update Profil

--- frontend/src/components/Profil.js
+++ frontend/src/components/Profil.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Typography, TableContainer, Table, TableHead, TableCell, Paper, TableRow, TableBody, Link, Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 import {LernpartnerAPI} from '../api';
 import ContextErrorMessage from './dialogs/ContextErrorMessage';
@@ -16,15 +17,15 @@ class Profil extends Component {
         this.state = {
             profil: null,
             gruppe: false,
-            vorname = null,
-            name = null,
-            semester = 0,
-            studiengang = None,
+            personVorname = null,
+            personName = null,
+            personSemester = 0,
+            personStudiengang = None,
             """lerngruppe = False --> reinnehmen oder nicht?"""
             main_person_id = None,
             personenprofil_id = None,
-            lernfaecher = [],
-            lernvorlieben = null """oder doch []?"""
+            personLernfaecher = null,
+            personLernvorlieben = null """oder doch []?"""
             loadingInProgress: false,
             loadingError: null,
         };
@@ -35,6 +36,16 @@ class Profil extends Component {
         this.getProfil();
   }
 
+/**   showVorschlagButtonClick = (event) => {
+      event.stopPropagation();
+      this.setState({
+        showVorschlag: true
+      });
+    }**/
+
+
+
+
     // API Anbindung um Profil vom Backend zu bekommen
     getPerson = () => {
       LernpartnerAPI.getAPI().getPerson(this.props.person.getID())
@@ -43,16 +54,21 @@ class Profil extends Component {
             person: personBO,
             personName: personBO.name,
             personVorname: personBO.vorname,
+            personSemester: personBO.semester,
+            personStudiengang: personBO.studiengang,
+            personProfilID: personBO.personenprofil_id,
             loadingInProgress: false,
             error: null,
           }))
           .catch(e =>
               this.setState({
-                person: null,
-                personName: null,
-                personVorname: null,
-                loadingInProgress: false,
-                error: e,
+            person: null,
+            personName: null,
+            personVorname: null,
+            personSemester: null,
+            personStudiengang: null,
+            loadingInProgress: false,
+            error: e,
               }));
       this.setState({
         loadingInProgress: true,
@@ -61,16 +77,20 @@ class Profil extends Component {
     }
 
      getProfil = () => {
-    LernpartnerAPI.getAPI().getProfil(this.props).then(profilBOs =>
+    LernpartnerAPI.getAPI().getProfil(this.props.profil.getID()).then(profilBO =>
       this.setState({
-        profil: profilBOs,
-        loadingInProgress: false,
-        loadingError: null
+            profil: profilBOs,
+            profilLernfaecher: profilBO.lernfaecher,
+            profilLernvorlieben: profilBO.lernvorlieben,
+            loadingInProgress: false,
+            error: null
       })).catch(e =>
         this.setState({ // Reset state with error from catch
           profil: null,
+          profilLernfaecher: null,
+          profilLernvorlieben: false,
           loadingInProgress: false,
-          loadingError: e
+          error: e,
         })
       );
 
@@ -83,18 +103,63 @@ class Profil extends Component {
 
    /** Renders the component */
   render() {
-    const { classes, profil } = this.props;
+    const { classes } = this.props;
     // Use the states customer
-    const { profil, loadingInProgress, loadingError} = this.state;
+    const { profil, name, vorname, semester, studiengang, lernfaecher, lernvorlieben, loadingInProgress, error} = this.state;
 
     // console.log(this.props);
     return (
       <div className={classes.root}>
+    """  <Button color="primary" onClick= {this.showVorschlagButtonClick}>Zurueck zu den Vorschlaegen</Button>"""
+      <Typography variant='body1' color={'textSecondary'}>
+
+                            <b>Semester: </b> {personSemester} <br />
+                            <b>Studiengang: </b>{personStudiengang}<br />
+                            <b>Lernfächer: </b>{personLernfaecher}<br />
+                            <b>Lernvorlieben: </b>{personLernvorlieben}<br />
+
 
       </div>
     );
   }
-
-
-
 }
+
+  const styles = theme => ({
+  root: {
+      width: '100%',
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      padding: theme.spacing(1),
+  },
+  content: {
+      margin: theme.spacing(1),
+    },
+  table: {
+      minWidth: 700,
+    },
+  formControl: {
+      margin: theme.spacing(1),
+      minWidth: 200,
+      textAlign: "left"
+  },
+
+  laden: {
+    padding: 0
+  },
+  breite: {
+    width: 220
+  }
+  });
+
+
+/** PropTypes */
+Profil.propTypes = {
+  /** @ignore */
+  classes: PropTypes.object.isRequired,
+  person: PropTypes.object.isRequired,
+  show: PropTypes.bool.isRequired
+}
+
+
+export default withStyles(styles)(Profil);
+
