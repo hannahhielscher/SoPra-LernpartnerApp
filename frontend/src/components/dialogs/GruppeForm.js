a79314b833fdfd6b820c207e23dd267bf951c222
AleynaOezdal <79413535+AleynaOezdal@users.noreply.github.com>
2021-06-17 15:52:19 +0200

    Create GruppeForm.js

---
+++ frontend/src/components/dialogs/GruppeForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, Button, IconButton, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import { TeachingbeeAPI, GroupBO } from '../../api';
+import ContextErrorMessage from './ContextErrorMessage';
+import LoadingProgress from './LoadingProgress';
+
+
+class GruppenForm extends Component {
+
+  constructor(props) { }
+
+}
